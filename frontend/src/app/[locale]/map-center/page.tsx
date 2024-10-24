694e0512286741233ed1b86cff7b8b2e3b816d1b
Tate Mauzy <tate@socialchain.app>
2024-10-24 12:31:30 -0700

    Use Suspense boundary to avoid 500 error on seller registration page load

--- frontend/src/app/[locale]/map-center/page.tsx
+++ frontend/src/app/[locale]/map-center/page.tsx
@@ -2,21 +2,26 @@
 
 import dynamic from 'next/dynamic';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-const MapCenterPage = () => {
+const MapCenter = () => {
   const searchParams = useSearchParams();
   const entryType = searchParams.get('entryType'); // Get 'entryType' from URL query params
 
   // Dynamically import the MapCenter component
-  const MapCenter = dynamic(() => import('@/components/shared/map/MapCenter'), {
+  const DynamicMapCenter = dynamic(() => import('@/components/shared/map/MapCenter'), {
     ssr: false,
   });
 
-  return (
-    <>
-      <MapCenter entryType={entryType as 'search' | 'sell'} /> {/* Pass entryType as a prop */}
-    </>
-  );
+  // Pass entryType as a prop
+  return <DynamicMapCenter entryType={entryType as 'search' | 'sell'} />
 };
+
+// Must wrap in a Suspense boundary to avoid 500 error on page load
+const MapCenterPage = () => (
+  <Suspense>
+    <MapCenter />
+  </Suspense>
+);
 
 export default MapCenterPage;
